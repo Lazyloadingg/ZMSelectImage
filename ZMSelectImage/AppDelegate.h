b44475034c645745237cbc4fe92849017e43fb32
shengguangdaren <w532080396@163.com>
2017-07-12 18:16:23 +0800

    Initial Commit

---
+++ ZMSelectImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZMSelectImage
+//
+//  Created by Mac2 on 2017/7/12.
+//  Copyright © 2017年 圣光大人. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZMSelectImage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZMSelectImage
+//
+//  Created by Mac2 on 2017/7/12.
+//  Copyright © 2017年 圣光大人. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
